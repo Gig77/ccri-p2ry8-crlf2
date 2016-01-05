a1051f619d148b7635ee2a35174bb7c208ab6d15
Gig77 <frech.christian@gmail.com>
2016-01-05 17:26:06 +0100

    v1

--- figures/clonal-kinetics-plot-xenografts.R
+++ figures/clonal-kinetics-plot-xenografts.R
@@ -15,6 +15,8 @@ library(reshape)
 #p <- "AL9890" ; sample.RR <- "rem_rel2" ; exclude.chr <- NA ; genes.to.label <- c("CRYZ", "SEC16B", "FAT1") ; max.af.plot <- 0.7
 #p <- "S23" ; sample.RR <- "rem_rel3" ; exclude.chr <- NA ; genes.to.label <- c("NPY2R", "CREBBP", "GPR63") ; max.af.plot <- 1
 
+label.genes <- c("P-C", "+21", "+X", "+Y", "-Y", "IKZF1", "IKZF2", "PAX5", "EBF1", "ETV6", "JAK2", "JAK3", "JAK1", "IL7R", "SYK", "CRLF2", "KRAS", "NRAS", "PTPN11", "CBL", "FLT3", "CDKN2A", "BTG1", "E2F3", "FOXP1", "AFF1", "XBP1", "TRRAP", "SETD2", "CREBBP", "KMT2D", "EP300", "USP9X", "BRCA2", "MET", "MSH6")
+
 input <- list(
 	"108" = list(
 		timepoints =   c("D",           "Dxg",                    "R",           "Rxg",                    "RR"),
@@ -22,7 +24,7 @@ input <- list(
 		xenograft =    c(FALSE,         TRUE,                     FALSE,         TRUE,                     FALSE),
 		transplant =   c(FALSE,         FALSE,                    FALSE,         FALSE,                    TRUE),
 		blast.counts = c("91",          "NA",                     "93",          "NA",                     "85"),
-		genes.to.label = c(""),
+		genes.to.label = label.genes,
 		exclude.chr = c("MT"),
 		max.af.plot = 1),
 	"715" = list(
@@ -31,7 +33,7 @@ input <- list(
 	  xenograft =    c(FALSE,         FALSE,         TRUE,                     FALSE),
 	  transplant =   c(FALSE,         FALSE,         FALSE,                    TRUE),
 	  blast.counts = c("92",          "92",          "NA",                     "NA"),
-	  genes.to.label = c(""),
+	  genes.to.label = label.genes,
 	  exclude.chr = c("MT"),
 	  max.af.plot = 1),
 	"737" = list(
@@ -40,8 +42,8 @@ input <- list(
     xenograft =    c(FALSE,         TRUE,                     FALSE,         TRUE,                     FALSE),
     transplant =   c(FALSE,         FALSE,                    FALSE,         FALSE,                    TRUE),
     blast.counts = c("97",          "NA",                     "96",          "NA",                     "NA"),
-    genes.to.label = c(""),
-    exclude.chr = c("MT", "X", "Y"),
+    genes.to.label = label.genes,
+    exclude.chr = c("MT"),
     max.af.plot = 1),
 	"545" = list(
 	  timepoints =   c("D",           "R",           "Rxg1",                   "Rxg2"),
@@ -49,7 +51,7 @@ input <- list(
 	  xenograft =    c(FALSE,         FALSE,         TRUE,                     TRUE),
 	  transplant =   c(FALSE,         FALSE,         FALSE,                    FALSE),
 	  blast.counts = c("97",           "86",         "NA",                      "NA"),
-	  genes.to.label = c(""),
+	  genes.to.label = label.genes,
 	  exclude.chr = c("MT"),
 	  max.af.plot = 1),
 	"839" = list(
@@ -58,7 +60,7 @@ input <- list(
 	  xenograft =    c(FALSE,         TRUE,                     FALSE),
 	  transplant =   c(FALSE,         FALSE,                    FALSE),
 	  blast.counts = c("92",           "NA",                    "86"),
-	  genes.to.label = c(""),
+	  genes.to.label = label.genes,
 	  exclude.chr = c("MT"),
 	  max.af.plot = 1),
 	"92" = list(
@@ -67,7 +69,7 @@ input <- list(
 	  xenograft =    c(FALSE,         TRUE,                   FALSE),
 	  transplant =   c(FALSE,         FALSE,                  FALSE),
 	  blast.counts = c("97",          "NA",                   "90"),
-	  genes.to.label = c(""),
+	  genes.to.label = label.genes,
 	  exclude.chr = c("MT"),
 	  max.af.plot = 1),
 	"841" = list(
@@ -76,7 +78,7 @@ input <- list(
 	  xenograft =    c(FALSE,         TRUE),
 	  transplant =   c(FALSE,         FALSE),
 	  blast.counts = c("98",          "NA"),
-	  genes.to.label = c(""),
+	  genes.to.label = label.genes,
 	  exclude.chr = c("MT"),
 	  max.af.plot = 1),
 	"379" = list(
@@ -85,7 +87,7 @@ input <- list(
 	  xenograft =    c(FALSE,         TRUE),
 	  transplant =   c(FALSE,         FALSE),
 	  blast.counts = c("97",          "NA"),
-	  genes.to.label = c(""),
+	  genes.to.label = label.genes,
 	  exclude.chr = c("MT"),
 	  max.af.plot = 1),
 	"833" = list(
@@ -94,7 +96,7 @@ input <- list(
 	  xenograft =    c(FALSE,         TRUE),
 	  transplant =   c(FALSE,         FALSE),
 	  blast.counts = c("96",          "NA"),
-	  genes.to.label = c(""),
+	  genes.to.label = label.genes,
 	  exclude.chr = c("MT"),
 	  max.af.plot = 1)
 )
@@ -110,7 +112,7 @@ data.xeno <- read.csv("/mnt/projects/p2ry8-crlf2/results/filtered-variants.xenog
 data <- rbind(data.prim, data.xeno)
 data <- data[!grepl("^GL", data$chr),]
 data <- data[data$status != "REJECT",]
-data <- data[data$non_silent==1,]
+data <- data[data$deleterious=="yes",]
 
 #data <- data[data$dp_leu_tot >= min.cov & data$dp_rem_tot >= min.cov,]  # minimum coverage filter
 #data <- data[data$dp_rem_tot < mean(data$dp_rem_tot) + cov.max.std.dev * sd(data$dp_rem_tot),] # maximum coverage filter
@@ -121,7 +123,13 @@ col.af <- "freq_leu"
 pdf(paste0("/mnt/projects/p2ry8-crlf2/results/figures/clonal-kinetics-xenografts.pdf"), width=12)
 
 for (p in names(input)) {
+
   data.patient <- NULL
+  num.tp <- length(input[[p]]$timepoints)
+  x <- seq(1.3, 1.5*(num.tp-1)+1.3, by=1.5)
+  
+  # merge allelic frequencies from all timepoints
+  
   for(i in 1:length(input[[p]]$timepoints)) {
     id <- input[[p]]$identifiers[i]
     tp <- input[[p]]$timepoints[i]
@@ -141,54 +149,80 @@ for (p in names(input)) {
     data.patient[is.na(data.patient[,tp]),tp] <- 0
   }
 
+  # exclude mutations from unwanted chromosomes
+  
   if (!is.null(input[[p]]$exclude.chr)) {
     data.patient <- data.patient[!(data.patient$chr %in% input[[p]]$exclude.chr),]  # sex chromosome filter to not distort AF extimates
   }
   
-  #xeno.col <- input[[p]]$timepoints[grepl("xeno", input[[p]]$identifiers)]
-  primary.col <- input[[p]]$timepoints[!input[[p]]$xenograft]
-  xeno.col <- input[[p]]$timepoints[input[[p]]$xenograft]
-
-  # only variants 
+  # filter mutations based on conservation pattern
+  # keep variants that are
   # a) conserved between at least two samples (counting xenografts as one) OR
   # b) predicted deleterious COSMIC variant with AF > 10% (excluding xenografts)
+  
+  primary.col <- input[[p]]$timepoints[!input[[p]]$xenograft]
+  xeno.col <- input[[p]]$timepoints[input[[p]]$xenograft]
   primary <- rowSums(data.patient[, primary.col, drop=FALSE] > 0)
   xeno <- rowSums(data.patient[, xeno.col, drop=FALSE] > 0) 
   conserved <- primary > 1 | (primary > 0 & xeno > 0)
   cosmic <- data.patient$cosmic_hits_nt > 0 & data.patient$deleterious=="yes" & apply(data.patient[,input[[p]]$timepoints], 1, max) >= 0.1
-  data.patient <- data.patient[conserved | (primary & cosmic),]
+  interesting <- data.patient$gene %in% label.genes & apply(data.patient[,input[[p]]$timepoints], 1, max) >= 0.1
+  data.patient <- data.patient[(primary & interesting) | conserved,]
   
   if (nrow(data.patient) == 0) {
     plot(0, 0, main=paste0(p, ": No mutations found"), xlab="", ylab="", xaxt='n', yaxt='n')
     next
   }
 
+  # color mutations by gene
+  data.patient$color <- c("#0080ff", "#ff00ff", "darkgreen", "#ff0000", "orange", "#00ff00", "brown")[as.integer(as.factor(as.character(data.patient$gene))) %% 6 + 1]
+  
   # define groups based on AF at each timepoint
-  group.conserved <- data.patient[rowSums(apply(data.patient[,input[[p]]$timepoints], 2, function(x) x > 0.2))>=4,]
-  group.notdia <- data.patient[data.patient$D == 0 & data.patient$Dxg == 0 & data.patient$R > 0.2 & data.patient$Rxg > 0.2 & data.patient$RR > 0.2,]
-  
-    # label genes conserved in at least two samples (all xenografts counted as one)
-  #genes.to.label <- unique(data.patient$gene[rowSums(apply(data.patient[,input[[p]]$timepoints], 2, function(x) x > 0)) > 1])
-  
-  # plot
-
+  #group.conserved <- data.patient[rowSums(apply(data.patient[,input[[p]]$timepoints], 2, function(x) x > 0.2))>=4,]
+  #group.notdia <- data.patient[data.patient$D == 0 & data.patient$Dxg == 0 & data.patient$R > 0.2 & data.patient$Rxg > 0.2 & data.patient$RR > 0.2,]
   data.patient$group <- "unassigned"
-  #data.patient$group[rowSums(apply(data.patient[,input[[p]]$timepoints], 2, function(x) x > 0))>=5] <- "conserved"
-  m <- melt(data.patient, id.vars=c(cols.merge, "group"))
-  m$mutation <- paste0(m$chr, ":", m$pos, ":", m$ref, ">", m$alt)
-  m$color <- as.integer(as.factor(as.character(m$gene)))
+  
+  # all mutations in one plot
+  
+  plot(0, 0, xlim=c(1, 1.5*(num.tp-1)+1.6), ylim=c(0, input[[p]]$max.af.plot), type="n", xaxt="n", yaxt="n", xlab="", ylab="Allelic frequency", main=paste(p, " (n=", nrow(data.patient), ")", sep=""))
+  abline(h=seq(0, 1, 0.1), lty=2, col="#eeeeee", lwd=0.5)
+  axis(1, at = x, labels=paste0(input[[p]]$timepoints, "\n(", input[[p]]$blast.counts, "% blasts)"), padj=0.5)
+  axis(2, at = seq(0, 1, 0.1), las = 1) 
+  
+  # draw group polygons
+  #polygon(c(x, rev(x)), c(apply(group.conserved[,input[[p]]$timepoints], 2, min), rev(apply(group.conserved[,input[[p]]$timepoints], 2, max))), col=rgb(1, 1, 0, 0.15), border=NA)
+  #polygon(c(x, rev(x)), c(apply(group.notdia[,input[[p]]$timepoints], 2, min), rev(apply(group.notdia[,input[[p]]$timepoints], 2, max))), col=rgb(0, 0, 1, 0.15), border=NA)
+  
+  for(i in 1:nrow(data.patient)) {
+    lines(x, data.patient[i,input[[p]]$timepoints], type="b", col=data.patient$color[i], pch=19, lwd=ifelse(data.patient$gene[i] %in% label.genes, 5, 1.5))
+    if (data.patient$gene[i] != "" & data.patient$gene[i] %in% input[[p]]$genes.to.label) {
+      text(1.25, data.patient[i,input[[p]]$timepoints[1]], paste0(data.patient$gene[i], " --"), adj=c(1, 0.5), cex=0.8)
+      text(1.5*(num.tp-1)+1.35, data.patient[i,tail(input[[p]]$timepoints,1)], paste0("-- ", data.patient$gene[i]), adj=c(0, 0.5), cex=0.8)
+    }
+  }	
+  
+  # lattice plot with mutations split by gene
+
+  m <- melt(data.patient, id.vars=c(cols.merge, "group", "color"))
+  m$mutation <- as.factor(paste0(m$chr, ":", m$pos, ":", m$ref, ">", m$alt))
+  gene.colors <- m$color[match(levels(m$mutation), m$mutation)] # arrange colors by grouping factor
   
   print(xyplot(value ~ variable | gene, 
                group=mutation, 
-               data=m[m$group=="unassigned",], 
-               type='b', 
-               par.settings=simpleTheme(pch=ifelse(m$deleterious=="yes", 19, 1)),
+               data=m, 
+               type='b',
+               par.settings=list(superpose.line = list(col=gene.colors), superpose.symbol = list(pch=19, col=gene.colors)),
                main=p, 
                as.table=TRUE, 
                par.strip.text=list(cex=0.7),
                xlab="Sample",
-               ylab="Allelic frequency")
-        )
+               ylab="Allelic frequency",
+               ylim=c(-0.1,1.1),
+               panel = function(...) {
+                 panel.abline(h=seq(0, 1, 0.1), col.line="#eeeeee", lty=2, lwd=0.5)
+                 panel.xyplot(...)
+               }
+        ))
 
   #plot.conserved <- xyplot(value ~ variable | gene, group=mutation, data=m[m$group=="conserved",], type='b', layout=c(1, 4))
   #plot.unassigned <- xyplot(value ~ variable | gene, group=mutation, data=m[m$group=="unassigned",], type='b', layout=c(2, 4, 1))
@@ -198,53 +232,5 @@ for (p in names(input)) {
 }
 
 dev.off()
-stop("OK")
 
-# base graphics
-
-num.tp <- length(input[[p]]$timepoints)
-x <- seq(1.3, 1.5*(num.tp-1)+1.3, by=1.5)
-
-plot(0, 0, xlim=c(1, 1.5*(num.tp-1)+1.6), ylim=c(0, input[[p]]$max.af.plot), type="n", xaxt="n", yaxt="n", xlab="", ylab="Allelic frequency", main=paste(p, " (n=", nrow(data.patient), ")", sep=""))
-axis(1, at = x, labels=paste0(input[[p]]$timepoints, "\n(", input[[p]]$blast.counts, "% blasts)"), padj=0.5)
-axis(2, at = seq(0, 1, 0.1), las = 1) 
-
-# draw group polygons
-polygon(c(x, rev(x)), c(apply(group.conserved[,input[[p]]$timepoints], 2, min), rev(apply(group.conserved[,input[[p]]$timepoints], 2, max))), col=rgb(1, 1, 0, 0.15), border=NA)
-polygon(c(x, rev(x)), c(apply(group.notdia[,input[[p]]$timepoints], 2, min), rev(apply(group.notdia[,input[[p]]$timepoints], 2, max))), col=rgb(0, 0, 1, 0.15), border=NA)
-
-# draw lines and gene names
-#par(mfrow=c(nrow(data.patient), 1), mar=c(0,3,0,1))
-for(i in 1:nrow(data.patient)) {
-  #plot(0, 0, xlim=c(1, 1.5*(num.tp-1)+1.6), ylim=c(0, input[[p]]$max.af.plot), type="n", xaxt="n", yaxt="n", xlab="", ylab="Allelic frequency", main=ifelse(i==1, paste(p, " (n=", nrow(data.patient), ")", sep=""), ""))
-  #if(i==nrow(data.patient)) {
-  #  axis(1, at = x, labels=paste0(input[[p]]$timepoints, "\n(", input[[p]]$blast.counts, "% blasts)"), padj=0.5)
-  #}
-  #axis(2, at = seq(0, 1, 0.1), las = 1) 
-  
-  lines(x, data.patient[i, (ncol(data.patient)-num.tp+1):ncol(data.patient)], type="b", col=i%%20, lwd=ifelse(data.patient$deleterious[i] == "yes", 4, 1))
-  
-  if (data.patient$gene[i] != "" & (data.patient$deleterious[i] == "yes" | data.patient$gene[i] %in% input[[p]]$genes.to.label)) {
-    text(1.25, data.patient[i, ncol(data.patient)-num.tp+1], paste0(data.patient$gene[i], " --"), adj=c(1, 0.5), cex=0.8)
-    text(1.5*(num.tp-1)+1.35, data.patient[i, ncol(data.patient)], paste0("-- ", data.patient$gene[i]), adj=c(0, 0.5), cex=0.8)
-  }
-}	
-
-# remove variants with AF < 10% at all timepoints (likely false positives)
-#m <- m[m$dia>=min.af | m$rel>=min.af | m$rel2>=min.af,]
-
-# remove variants with AF > 70% (likely inaccurate measurement)
-#m <- m[m$dia<=max.af & m$rel<=max.af & m$rel2<=max.af,]
-
-# assign colors depending on AFs
-#m$col <- "#000000"
-#m$col[m$dia>0.25 & m$rel>0.1 & m$rel2>0.25] <- "#C6C7C8"
-#m$col[m$dia>0.25 & m$rel==0 & m$rel2==0] <- "#4A8ECC"
-#m$col[m$dia>0.25 & m$rel>0 & m$rel<=0.25 & m$rel2==0] <- "#231F20"
-#m$col[m$dia==0 & m$rel>0.25 & m$rel2 > 0.25] <- "#C9252B"
-#m$col[m$dia==0 & m$rel>0.25 & m$rel2 == 0] <- "#76C58F"
-#m$col[m$dia==0 & m$rel<=0.25 & m$rel2 == 0] <- "#548A2F"
-#m$col[m$dia==0 & m$rel<=0.25 & m$rel2 > 0.25] <- "#F58E7D"
-#m$col[m$dia==0 & m$rel==0 & m$rel2 > 0.25] <- "#BCAFD6"
-#m$col[m$dia==0 & m$rel==0 & m$rel2 <= 0.25] <- "#FFF57C"
 
