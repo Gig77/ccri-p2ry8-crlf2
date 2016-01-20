db60ea9c7bba94e5c5a6edd5de89e03e9886740d
Gig77 <frech.christian@gmail.com>
2016-01-20 16:31:12 +0100

    v6

--- figures/clonal-kinetics-plot.R
+++ figures/clonal-kinetics-plot.R
@@ -5,16 +5,6 @@ library(lattice)
 library(gridExtra)
 library(reshape)
 
-# xenografts diagnosis: m1977-G-dia m1966-Y-dia m1035-108-dia m252-379-dia m1041-737-dia m247-833-dia m1059-92-dia m1037-839-dia m248-841-dia
-# xenografts relapse: m1963-545-rel m1957-715-rel m1967-Y-rel m1060-108-rel m1069-737-rel
-# 2nd xenograft relapse: m1964-545-rel 
-
-# 737 hat im 2. relapse nur 8% blasten, deshalb ausgenommen von der klonalen analyse
-#p <- "737" ; sample.RR <- "rem_rel3" ; exclude.chr <- c("X", "Y") ; genes.to.label <- c("CREBBP", "KRAS", "NRAS", "FLT3", "PTPN11", "FGFR2", "EPHA5") ; ; max.af.plot <- 0.7
-#p <- "108" ; sample.RR <- "rem_rel2" ; exclude.chr <- NA ; genes.to.label <- c("JAK2", "KMT2D", "SI", "MEGF6", "NBEA", "RPS5", "BIRC6", "SLC26A2") ; max.af.plot <- 0.7
-#p <- "AL9890" ; sample.RR <- "rem_rel2" ; exclude.chr <- NA ; genes.to.label <- c("CRYZ", "SEC16B", "FAT1") ; max.af.plot <- 0.7
-#p <- "S23" ; sample.RR <- "rem_rel3" ; exclude.chr <- NA ; genes.to.label <- c("NPY2R", "CREBBP", "GPR63") ; max.af.plot <- 1
-
 label.genes <- c("PAR1", "21", "X", "Y", "IKZF1", "IKZF2", "PAX5", "EBF1", "ETV6", "JAK2", "JAK3", "JAK1", "IL7R", "SYK", "CRLF2", "KRAS", "NRAS", "PTPN11", "CBL", "FLT3", "CDKN2A", "BTG1", "E2F3", "FOXP1", "AFF1", "XBP1", "TRRAP", "SETD2", "CREBBP", "KMT2D", "EP300", "USP9X", "BRCA2", "MET", "MSH6")
 
 input <- list(
@@ -144,6 +134,7 @@ min.cov <- 0
 cov.max.std.dev <- 10
 max.af <- 1
 min.af <- 0
+lty.emerging <- "21"
 
 # mutations
 mut.prim <- read.csv("/mnt/projects/p2ry8-crlf2/results/filtered-variants.cosmic.tsv", sep="\t")
@@ -151,7 +142,7 @@ mut.xeno <- read.csv("/mnt/projects/p2ry8-crlf2/results/filtered-variants.xenogr
 mut <- rbind(mut.prim, mut.xeno)
 mut <- mut[!grepl("^GL", mut$chr),]
 mut <- mut[mut$status != "REJECT",]
-mut <- mut[mut$deleterious=="yes",]
+mut <- mut[mut$deleterious=="yes" | mut$var_type == "indel",]
 mut <- mut[mut$gene %in% label.genes,]
 mut <- mut[,c("patient", "sample", "chr", "pos", "ref", "alt", "gene", "freq_leu", "cosmic_hits_aa")]
 mut$freq_leu.raw <- mut$freq_leu
@@ -167,23 +158,25 @@ for (p in names(input)) {
 	}
 }
 mut$freq_leu[mut$freq_leu>1] <- 1
+mut$freq_leu[mut$patient=="737" & mut$sample=="rem_rel3" & mut$chr=="9" & mut$pos=="36882082"] <- 0.9 # PAX5
+mut$freq_leu[mut$patient=="m1957-715-rel" & mut$sample=="rem_xeno" & mut$chr=="12" & mut$pos=="25398284"] <- 0.59 # KRAS
+mut$freq_leu[mut$patient=="m1957-715-rel" & mut$sample=="rem_xeno" & mut$chr=="16" & mut$pos=="3786787"] <- 0.45 # CREBBP
+
 
 # cnvs
 cnv <- read.csv("/mnt/projects/p2ry8-crlf2/data/cnvs_kinetics_figure.txt", sep="\t")
 
-#mut <- mut[mut$dp_leu_tot >= min.cov & mut$dp_rem_tot >= min.cov,]  # minimum coverage filter
-#mut <- mut[mut$dp_rem_tot < mean(mut$dp_rem_tot) + cov.max.std.dev * sd(mut$dp_rem_tot),] # maximum coverage filter
-
 cols.merge <- c("chr", "pos", "ref", "alt", "gene", "cosmic_hits_aa")
 col.af <- "freq_leu"
 
-pdf(paste0("/mnt/projects/p2ry8-crlf2/results/figures/clonal-kinetics.pdf"), width=8, height=6)
+pdf(paste0("/mnt/projects/p2ry8-crlf2/results/figures/clonal-kinetics.pdf"), width=4.9/2.54, height=3.5/2.54)
+par(mar=c(1,1,1,1), oma=c(0,0,0,0))
 
 for (p in names(input)) {
 	
 	mut.patient <- NULL
 	num.tp <- length(input[[p]]$timepoints)
-	x <- 1.8+1.5*(input[[p]]$xpos-1)
+	x <- 3.1+1.5*(input[[p]]$xpos-1)
 	
 	# merge allelic frequencies from all timepoints
 	
@@ -221,60 +214,82 @@ for (p in names(input)) {
 	mut.genes <- as.factor(as.character(mut.patient$gene))
 	colors <- c("#0080ff", "#0000ff", "darkgreen", "#ff0000", "orange", "#00ff00", "brown")
 	mut.patient$color <- colors[as.integer(mut.genes) %% (length(colors)-1) + 1]
-	
+
 	# all mutations in one plot
-	
-	plot(0, 0, xlim=c(0.5, max(x)+0.6), ylim=c(-0.08-0.03*length(unique((cnv$gene[cnv$patient==p]))), input[[p]]$max.af.plot), type="n", xaxt="n", yaxt="n", bty="n", xlab="", ylab="CNA                 Mutations (adj. allelic frequency)", main=ifelse(input[[p]]$title.suffix != "", paste(p, input[[p]]$title.suffix), p))
-	abline(h=seq(0.1, 1, 0.1), lty=2, col="#eeeeee", lwd=1)
+	plot(0, 0, 
+	     xlim=c(0.5, max(x)+0.6), 
+	     ylim=c(-0.03-0.1*length(unique(cnv$gene[cnv$patient==p])), 
+	     input[[p]]$max.af.plot), 
+	     type="n", xaxt="n", yaxt="n", bty="n", 
+	     xlab="", 
+	     ylab="CNA                             Mutations (AF)               ")
+	title(ifelse(input[[p]]$title.suffix != "", paste(p, input[[p]]$title.suffix), p), line=0.15, cex.main=0.6)
+	abline(h=seq(0.2, 1, 0.1), lty=2, col="#eeeeee", lwd=1)
 	abline(v=x, lty=2, col="black", lwd=1)
-	abline(h=0.3, lty=1, col="#bbbbbb", lwd=2)
+	abline(h=0.3, lty=1, col="#bbbbbb", lwd=1.5)
 	abline(h=-0.05)
 	box()
-	axis(1, at = x, labels=input[[p]]$timepoints, padj=0.5)
-	axis(2, at = seq(0, 1, 0.1), las = 1)
-	legend("topleft", levels(mut.genes), cex = 0.8, fill = colors[(1:length(levels(mut.genes))) %% (length(colors)-1) + 1], bg = "white")
+	axis(1, at = x, labels=input[[p]]$timepoints, padj=-1.7, mgp=c(3, 0.05, 0), tck=-0.02, cex.axis = 0.45)
+	axis(2, at = seq(0, 1, 0.1), las = 1, mgp=c(3, 0.25, 0), tck=-0.02, cex.axis = 0.45)
+	legend("topleft", 
+	       levels(mut.genes), 
+	       cex = 0.3, 
+	       border=colors[(1:length(levels(mut.genes))) %% (length(colors)-1) + 1], 
+	       fill = colors[(1:length(levels(mut.genes))) %% (length(colors)-1) + 1], 
+	       bg = "white")
 	
 	# mutations
 	
 	for(i in 1:nrow(mut.patient)) {
 		
 		# primary kinetics
-		if (max(mut.patient[i,input[[p]]$timepoints][!input[[p]]$xenograft]) > 0) {
-			lines(x[!input[[p]]$xenograft], mut.patient[i,input[[p]]$timepoints][!input[[p]]$xenograft], type="b", col=mut.patient$color[i], pch=ifelse(mut.patient[i,input[[p]]$timepoints][!input[[p]]$xenograft] > 0, 19, 1), lwd=3.5, cex=1.5)
+	  
+	  # plot mutations with low coverage and thus inaccruate AF estimates with open circle
+	  low.coverage <- rep(FALSE, sum(!input[[p]]$xenograft))
+	  if (p == "737" && mut.patient$chr[i]=="9" && mut.patient$pos[i]=="36882082") low.coverage[3] <- TRUE
+
+	  tp.primary <- input[[p]]$timepoints[!input[[p]]$xenograft]
+		if (length(tp.primary) > 1 && max(mut.patient[i,tp.primary]) > 0) {
+		  for (j in 2:length(tp.primary)) {
+		    lines(x[!input[[p]]$xenograft][(j-1):j], mut.patient[i,tp.primary[(j-1):j]], col=mut.patient$color[i], lty=ifelse(mut.patient[i,tp.primary[j-1]] == 0, lty.emerging, 1), lwd=1.2, cex=0.6)
+		  }
+		  points(x[!input[[p]]$xenograft], mut.patient[i,input[[p]]$timepoints][!input[[p]]$xenograft], col=mut.patient$color[i], pch=ifelse(mut.patient[i,input[[p]]$timepoints][!input[[p]]$xenograft] > 0 & !low.coverage, 19, 1), cex=0.6)
 		}
 		
 		# xenograft kinetics
 		for (s in which(input[[p]]$xenograft)) {
 			xeno.col <- match(input[[p]]$timepoints[s], names(mut.patient))
 			if (max(mut.patient[i,(xeno.col-1):xeno.col]) == 0) next
-			lines(x[(s-1):s], mut.patient[i,(xeno.col-1):xeno.col], type="b", col=mut.patient$color[i], pch=ifelse(mut.patient[i,(xeno.col-1):xeno.col] > 0, 19, 1), lwd=1.5, lty=1, cex=1.5)
-		}
-		if (mut.patient$gene[i] != "" & mut.patient$gene[i] %in% input[[p]]$genes.to.label) {
-			#text(1.5, mut.patient[i,input[[p]]$timepoints[1]], paste0(mut.patient$gene[i], " --"), adj=c(1, 0.5), cex=0.8)
-			#text(max(x)+0.35, mut.patient[i,tail(input[[p]]$timepoints,1)], paste0("-- ", mut.patient$gene[i]), adj=c(0, 0.5), cex=0.8)
+			lines(x[(s-1):s], mut.patient[i,(xeno.col-1):xeno.col], col=mut.patient$color[i], lty=ifelse(mut.patient[i,xeno.col-1] == 0, lty.emerging, 1), lwd=0.6, cex=0.6)
+			points(x[(s-1):s], mut.patient[i,(xeno.col-1):xeno.col], col=mut.patient$color[i], pch=ifelse(mut.patient[i,(xeno.col-1):xeno.col] > 0, 19, 1), lwd=0.6, cex=0.4)
 		}
 	}	
-	rect(par("usr")[1]+0.02, -0.048, par("usr")[2]-0.02, 0.098, col="#eeeeeeaa", border=NA)
-	#lines(c(par("usr")[1]+0.02, par("usr")[2]-0.02), c(0.097, 0.097), col="#aaaaaa", lwd=0.5)
-	for(i in 1:length(x)) {
-		lines(c(x[i], x[i]), c(0.1, -0.05), lty=2, col="black", lwd=1)
+	
+	# mark special mutations
+	if (p == "715") {
+	  points(x[2:4], mut.patient[mut.patient$pos==3786787, c("R", "RX", "RR")], col="white", pch=4, cex=0.25, lwd=0.6)
 	}
 	
+	rect(par("usr")[1]+0.06, -0.04, par("usr")[2]-0.06, 0.1, col="#eeeeeeaa", border=NA)
+
 	# cnvs
 	cnv.patient <- cnv[paste(cnv$patient, cnv$sample) %in% input[[p]]$identifiers,]
-	y <- -0.1
+	y <- -0.12
 	for (gene in unique(cnv.patient$gene)) {
 		lines(c(x[1], x[length(x)]), c(y, y), lty=2, col="#cccccc", lwd=1)
 		for (i in which(cnv.patient$gene==gene)) {
-			print(paste(cnv.patient$patient[i], cnv.patient$sample[i]))
-			print(input[[p]]$xpos[match(paste(cnv.patient$patient[i], cnv.patient$sample[i]), input[[p]]$identifiers)])
 			xpos <- x[match(paste(cnv.patient$patient[i], cnv.patient$sample[i]), input[[p]]$identifiers)]
 			cnv.col <- ifelse(cnv.patient$event[i] %in% c("deletion", "IK6"), "#0000ff", "red")
 			if (cnv.patient$gene[i] == "PAR1") cnv.col <- "darkblue"
-			rect(xpos-0.5, y+0.012, xpos+0.5, y-0.012, col=cnv.col, border=NA)
-			text(1.2, y, gene, adj=c(1, 0.5), cex=0.65)
+			cnv.col <- ifelse(cnv.patient$event[i] == "n/a", "white", cnv.col)
+			border.col <- ifelse(cnv.patient$event[i] == "n/a", "black", NA)
+			rect(xpos-0.5, y+0.04, xpos+0.5, y-0.04, col=cnv.col, border=border.col, lwd=0.7)
+			if (cnv.patient$event[i] == "n/a") {
+			  text(xpos, y+0.003, "n/a", col="black", cex=0.3)
+			}
+			text(2.5, y, gene, adj=c(1, 0.5), cex=0.4)
 		}
-		y <- y - 0.03
+		y <- y - 0.1
 	}
 	
 	next
@@ -301,12 +316,6 @@ for (p in names(input)) {
 						panel.xyplot(...)
 					}
 			))
-	
-	#plot.conserved <- xyplot(value ~ variable | gene, group=mutation, data=m[m$group=="conserved",], type='b', layout=c(1, 4))
-	#plot.unassigned <- xyplot(value ~ variable | gene, group=mutation, data=m[m$group=="unassigned",], type='b', layout=c(2, 4, 1))
-	#grid.arrange(plot.conserved, plot.unassigned, ncol=length(unique(m$group)))
-	
-	#write.table(m, file=paste0("/mnt/projects/p2ry8-crlf2/results/figures/clonal-kinetics-", p, ".tsv"), col.names=T, row.names=F, sep="\t", quote=F)
 }
 
 dev.off()
